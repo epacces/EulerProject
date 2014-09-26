f1a55ceac502d560e924493a4279b4173c6b0a4c
Eriprando Pacces <eriprando.pacces@gmail.com>
2014-09-26 22:40:06 +0100

    Initial Commit

---
+++ EulerProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EulerProject
+//
+//  Created by hepaKKes on 26/09/14.
+//  Copyright (c) 2014 Eriprando Pacces. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
